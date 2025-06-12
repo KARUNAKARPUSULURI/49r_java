ab4b0d588adc090268082f905cc3eb8b7b60301b
KARUNAKARPUSULURI <karunpusuluri@gmail.com>
2025-06-12 15:40:14 +0530

    explanation of program

---
+++ Explanation_of_first_program/PersonDetails.java
@@ -0,0 +1,27 @@
+
+public class PersonDetails{
+	public static void main(){
+	PersonDetails personDetails = new PersonDetails();
+		System.out.print("Hello");
+	} 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
