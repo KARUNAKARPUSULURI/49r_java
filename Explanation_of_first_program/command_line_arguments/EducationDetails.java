fb5a14f0eb9fe514ba977e783a3d11da3abca2b6
KARUNAKARPUSULURI <karunpusuluri@gmail.com>
2025-06-13 15:35:01 +0530

    command line arguments

---
+++ Explanation_of_first_program/command_line_arguments/EducationDetails.java
@@ -0,0 +1,14 @@
+class EducationalDetails{
+	public static void main(String[] args){
+		System.out.print("Student Name : ");
+		System.out.println(args[0]);
+		System.out.print("Student Age : ");
+		System.out.println(args[1]);
+		System.out.print("Student Email : ");
+		System.out.println(args[2]);
+		System.out.print("Phone Number : ");
+		System.out.println(args[3]);
+		System.out.print("Marks : ");
+		System.out.println(args[4]);
+	}
+}
---
+++ Explanation_of_first_program/command_line_arguments/PersonDetails.java
@@ -0,0 +1,25 @@
+public class PersonDetails{
+	public static void main(String [] args){
+	PersonDetails personDetails = new PersonDetails();
+		System.out.print("Name : ");
+		System.out.println(args[0]); //D
+		System.out.print("Age : ");
+		System.out.println(args[1]); //D
+		System.out.print("Gender : ");
+		System.out.print(args[2]); //D
+	} 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
