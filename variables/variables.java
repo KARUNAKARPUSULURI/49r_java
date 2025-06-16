a87b0a90187a371560f70270d47c64351b10b8ea
KARUNAKARPUSULURI <karunpusuluri@gmail.com>
2025-06-16 15:39:23 +0530

    variables

---
+++ variables/variables.java
@@ -0,0 +1,14 @@
+class Variables{
+	int age = 30;
+	public void DisplayInfo(){
+		int pincode = 430030;
+		System.out.print("5" + pincode);
+	}
+	public static void main(String [] args){
+                String n&ame = "Karunakar";
+		String bio = "asdfghjhgfds";
+		System.out.print("9" + n&ame + bio);
+		Variables personDetails = new Variables();
+		personDetails.DisplayInfo();
+	}
+}
